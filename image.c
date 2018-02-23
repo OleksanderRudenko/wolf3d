cac38100661a2e6844836f62c39e5395eb5c7ce0
Rudick <rudenkoolexandr@gmail.com>
2018-02-23 19:12:49 +0200

    Update image.c

--- image.c
+++ image.c
@@ -1,10 +1,10 @@
 #include "woof.h"
 
-void	get_image(t_wolf *one)
+SDL_Surface *load_surf(char *filepath)
 {
-	one->mal = (t_put_image *)malloc(sizeof(t_put_image));
-	one->mal->d_pic = mlx_new_image(one->mlx, WIDTH, HEIGHT);
-	one->mal->pic = mlx_get_data_addr(one->mal->d_pic, &one->mal->bpp, &one->mal->s_l, &one->mal->end);
-	one->mal->bpp /= 8;
-	one->mal->color = mlx_get_color_value(one->mlx, 0xffff);
+	SDL_Surface * surf;
+	surf  = SDL_LoadBMP(filepath);
+	if (surf == NULL)
+		ft_putendl(SDL_GetError());
+	return (surf);
 }
